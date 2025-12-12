900f8b6d914a855704bfe2ef7a113bdaba12c18c
Talha Yusif <talhaysif06@gmail.com>
2025-12-12 18:58:43 +0000

    one

---
+++ MySQLServer.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+try:
+    connection=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='Physics3939$'
+    
+    
+    )
+    create_database="""
+    CREATE DATABASE IF NOT EXISTS alx_book_store
+    """
+    
+
+    cursor=connection.cursor()
+    cursor.execute(create_database)
+    print(" Database 'alx_book_store' created successfully!")
+    cursor.close()
+   
+except Error as e :
+    print(e)
+finally:
+    if connection.is_connected():
+        connection.close()
